16abd94c89178a851619d575ac937d42f9345a5e
andronik <costellationoblivion@gmail.com>
2023-04-01 10:05:43 +0300

    changed LinkBaseEntity in BLL

--- SchoolLibrary/SchoolLibrary_EF.BLL/DTO/HATEOAS/LinkBaseEntity.cs
+++ SchoolLibrary/SchoolLibrary_EF.BLL/DTO/HATEOAS/LinkBaseEntity.cs
@@ -1,6 +1,6 @@
 ﻿namespace SchoolLibrary_EF.BLL.DTO.HATEOAS
 {
-    public class LinkBaseEntity
+    public abstract class LinkBaseEntity
     {
         public IList<Link> Links = new List<Link>();
     }
