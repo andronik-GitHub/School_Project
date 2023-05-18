eea70d93a35fc5282e0d027b3c2fd7cb43f98738
andronik <costellationoblivion@gmail.com>
2023-05-18 15:44:52 +0300

    Create Features RegisterModerator for UserIdentity use CQRS

---
+++ SchoolLibrary/SchoolLibrary_CA/Src/Application/Features/UserIdentityFeatures/Commands/RegisterModeratorIdentity/RegisterModeratorIdentityCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.Features.UserIdentityFeatures.Commands.RegisterModeratorIdentity
+{
+    public class RegisterModeratorIdentityCommand : IRequest<Guid>
+    {
+        public string UserName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
---
+++ SchoolLibrary/SchoolLibrary_CA/Src/Application/Features/UserIdentityFeatures/Commands/RegisterModeratorIdentity/RegisterModeratorIdentityCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Interfaces;
+using MediatR;
+
+namespace Application.Features.UserIdentityFeatures.Commands.RegisterModeratorIdentity
+{
+    public class RegisterModeratorIdentityCommandHandler
+        : IRequestHandler<RegisterModeratorIdentityCommand, Guid>
+    {
+        private readonly ISchoolLibraryContext _context;
+        private readonly IUserManager _userManager;
+
+        public RegisterModeratorIdentityCommandHandler(ISchoolLibraryContext context, IUserManager userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+
+        public async Task<Guid> Handle(RegisterModeratorIdentityCommand command, CancellationToken cancellationToken)
+        {
+            var id = await _userManager.RegisterModeratorAsync(command.UserName, command.Email, command.Password);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return id;
+        }
+    }
+}
