67fea8d41fc9490118af0df9b89fbae2416746ba
andronik <costellationoblivion@gmail.com>
2023-05-22 13:50:02 +0300

    Fixed DI for authentication, now jwt token works

--- SchoolLibrary/SchoolLibrary_CA/Src/Infrastructure/DependencyInjection.cs
+++ SchoolLibrary/SchoolLibrary_CA/Src/Infrastructure/DependencyInjection.cs
@@ -15,60 +15,67 @@ namespace Infrastructure
     {
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            // Configuration from appsettings.json
-            services.Configure<JWT>(configuration.GetSection("JWT"));
-            
-            services
-                // Register ASP.NET Core Identity with method AddIdentity<TUser, TRole>
-                .AddIdentity<UserIdentity, IdentityRole<Guid>>()
-                // To register the required EF Core implementation of Identity stores
-                .AddEntityFrameworkStores<IdentitySchoolLibraryDbContext>()
-                .AddDefaultTokenProviders();
-            services.AddScoped<IUserManager, UserManagerService>(); // Register UserManager
-            
-            
-            // Register IdentityDbContext
-            services.AddDbContext<IdentitySchoolLibraryDbContext>(options =>
+            #region DbContext
             {
-                options
-                    .UseSqlServer(
-                        configuration.GetConnectionString("sqlConnection"),
-                        b => b.MigrationsAssembly(typeof(IdentitySchoolLibraryDbContext).Assembly.GetName().Name)
-                    )
-                    .EnableSensitiveDataLogging();
-            });
-            
-            
-            // Register Authentication - JWT
-            services
-                // Adding Authentication
-                .AddAuthentication(options =>
+                // Register IdentityDbContext
+                services.AddDbContext<IdentitySchoolLibraryDbContext>(options =>
                 {
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                // Adding Scheme JwtBearer
-                .AddJwtBearer(options =>
-                {
-                    options.RequireHttpsMetadata = false;
-                    options.SaveToken = false;
-                    
-                    // Parameters for validating JWT tokens
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        ValidateIssuer = true,
-                        ValidateAudience = true,
-                        ValidateLifetime = true, // whether lifetime will be validated
-                        // Фllow time deviation when checking JWT token expiration
-                        ClockSkew = TimeSpan.Zero,
-                        
-                        ValidIssuer = configuration["JWT:Issuer"] ?? "SecureCA_Api",
-                        ValidAudience = configuration["JWT:Audience"] ?? "SecureCA_ApiUser",
-                        IssuerSigningKey = new SymmetricSecurityKey
-                            (Encoding.UTF8.GetBytes(configuration["JWT:Key"] ?? "C1CA1B7DC4C4175B6288DE4F55CA4")),
-                    };
+                    options
+                        .UseSqlServer(
+                            configuration.GetConnectionString("sqlConnection"),
+                            b => b.MigrationsAssembly(typeof(IdentitySchoolLibraryDbContext).Assembly.GetName().Name)
+                        )
+                        .EnableSensitiveDataLogging();
                 });
+            }
+            #endregion
+
+            #region Identity
+            {
+                // Configuration from appsettings.json
+                services.Configure<JWT>(configuration.GetSection("JWT"));
+                
+                services
+                    // Register ASP.NET Core Identity with method AddIdentity<TUser, TRole>
+                    .AddIdentity<UserIdentity, IdentityRole<Guid>>()
+                    // To register the required EF Core implementation of Identity stores
+                    .AddEntityFrameworkStores<IdentitySchoolLibraryDbContext>()
+                    .AddDefaultTokenProviders();
+                services.AddScoped<IUserManager, UserManagerService>(); // Register UserManager
+                
+                
+                // Register Authentication - JWT
+                services
+                    // Adding Authentication
+                    .AddAuthentication(options =>
+                    {
+                        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    })
+                    // Adding Scheme JwtBearer
+                    .AddJwtBearer(options =>
+                    {
+                        options.SaveToken = true;
+                        options.RequireHttpsMetadata = false;
+                        
+                        // Parameters for validating JWT tokens
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            // ValidateIssuerSigningKey = true,
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true, // whether lifetime will be validated
+                            // Фllow time deviation when checking JWT token expiration
+                            ClockSkew = TimeSpan.Zero,
+                            
+                            ValidIssuer = configuration["JWT:Issuer"] ?? "SecureCA_Api",
+                            ValidAudience = configuration["JWT:Audience"] ?? "SecureCA_ApiUser",
+                            IssuerSigningKey = new SymmetricSecurityKey
+                                (Encoding.UTF8.GetBytes(configuration["JWT:Key"] ?? "C1CA1B7DC4C4175B6288DE4F55CA4")),
+                        };
+                    });
+            }
+            #endregion
         }
     }
 }
