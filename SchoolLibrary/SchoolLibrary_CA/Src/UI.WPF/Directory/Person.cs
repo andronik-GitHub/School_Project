fad58213ab9c87d6a4eda07efdf72e0a198e6081
andronik <costellationoblivion@gmail.com>
2023-06-06 19:12:55 +0300

    Created a main page and a login/registration page in Figma. Started implementing main page in WPF based on a design created in Figma

---
+++ SchoolLibrary/SchoolLibrary_CA/Src/UI.WPF/Directory/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace UI.WPF.Directory
+{
+    public class Person
+    {
+        public string Name { get; set; } = default!;
+        public int Age { get; set; } = default!;
+
+        public override string ToString()
+        {
+            return $"{Name} {Age}";
+        }
+    }
+}
---
+++ SchoolLibrary/SchoolLibrary_CA/Src/UI.WPF/Directory/Phone.cs
@@ -0,0 +1,15 @@
+﻿namespace UI.WPF.Directory
+{
+    public class Phone
+    {
+        public string Title { get; set; } = default!;
+        public string Company { get; set; } = default!;
+        public int Price { get; set; } = default!;
+
+
+        public override string ToString()
+        {
+            return $"{Title} ({Company}) - {Price}";
+        }
+    }
+}
