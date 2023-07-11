d7809b8b1727cce88b8372b4a95322564ea468b7
andronik <costellationoblivion@gmail.com>
2023-07-11 13:15:12 +0300

    In the Dapper project made changes to the system for deleting and updating records in the database

--- SchoolLibrary/SchoolLibrary_EF/SchoolLibrary_EF.DAL/Entities/BaseEntity.cs
+++ SchoolLibrary/SchoolLibrary_EF/SchoolLibrary_EF.DAL/Entities/BaseEntity.cs
@@ -1,16 +1,11 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
-
-namespace SchoolLibrary_EF.DAL.Entities
+﻿namespace SchoolLibrary_EF.DAL.Entities
 {
     public class BaseEntity
     {
-        [NotMapped]
-        [JsonIgnore]
         public Guid Id { get; set; }
         
         public DateTime DateCreated { get; set; }
-        public DateTime? DateUpdated { get; set; }
-        public DateTime? DateDeleted { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeleteDate { get; set; }
     }
 }
